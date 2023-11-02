20bfd4c0ec6a92fe5abea0d8624e537cdd4dd3a0
Kiteka2 <k.iteka@alustudent.com>
2023-11-02 10:28:35 -0700

    7-edges.py

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton"
-
-
-
+word = "school"
 print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[7:]}")
+# prints first 3 letters.
+print(f"Last 2 letters: {word[4:]}")
+# prints last 2 letters.
 print(f"Middle word: {word[1:-1]}")
+# prints middle letters
