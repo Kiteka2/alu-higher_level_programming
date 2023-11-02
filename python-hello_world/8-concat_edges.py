478e71691071e848b5445743fd9f5465b4734467
Kiteka2 <k.iteka@alustudent.com>
2023-11-02 11:48:52 -0700

    last

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# python
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67] + " with Python")
+print(str[48:], end="\n")
