4e21248516880520815488109e5eb4ff4f5fe260
Kiteka2 <k.iteka@alustudent.com>
2023-11-23 21:21:17 +0200

    trying to fix errors

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''A module containing a locked class'''
+
+
+class LockedClass:
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ['first_name']
